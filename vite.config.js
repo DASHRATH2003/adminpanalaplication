4fa756def16169723f2c4f32f86bfde26ce95df7
Your Name <your.email@example.com>
2025-09-05 20:48:33 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['xlsx']
-    }
-  },
-  resolve: {
-    alias: {
-      'xlsx': resolve(__dirname, 'node_modules/xlsx')
-    }
+  optimizeDeps: {
+    include: ["xlsx"]   // 👈 force pre-bundle
   }
 })
